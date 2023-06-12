bfd9d13d9a79b44cb16bb43007696380f04669cb
Mariia Bondar <param.pam.pam.mariia@gmail.com>
2023-06-12 18:49:30 +0300

    +

--- src/components/OrderList/OrderList.jsx
+++ src/components/OrderList/OrderList.jsx
@@ -15,48 +15,51 @@ const OrderList = () => {
 
     return (
         <div className={style.list}>
-            <div className={style.info}>
+            <div className={style.item}>
                 <div className={style.pic}></div>
-                <div className={style.text}>
-                    <p>name {itemName}</p>
-                    <p>Price: {price}</p>
-                </div>
-                <div className={style.number}>
-                    <div className={style.values}>
-                        {value}
+                <div className={style.info}>
+                    <div className={style.text}>
+                        <p>name {itemName}</p>
+                        <p>Price: {price}</p>
                     </div>
-                    <div className={style.arr}>
-                        <button type="button" onClick={() => setValue(value + 1)}>
-                            <svg
-                                width="32"
-                                height="22"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
+                    <div className={style.number}>
+                        <div className={style.values}>
+                            {value}
+                        </div>
+                        <div className={style.arr}>
+                            <button type="button" onClick={() => setValue(value + 1)}>
+                                <svg
+                                    width="32"
+                                    height="22"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
 
-                                <path
-                                    d="M22.086 20.914l2.829-2.829-8.914-8.914-8.914 8.914 2.828 2.828 6.086-6.086z"
-                                    fill="#0c0c0d"
-                                />
-                            </svg>
-                        </button>
-                        <button type="button" onClick={() => setValue(value - 1)}>
-                            <svg
-                                width="32"
-                                height="22"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
+                                    <path
+                                        d="M22.086 20.914l2.829-2.829-8.914-8.914-8.914 8.914 2.828 2.828 6.086-6.086z"
+                                        fill="#0c0c0d"
+                                    />
+                                </svg>
+                            </button>
+                            <button type="button" onClick={() => setValue(value - 1)}>
+                                <svg
+                                    width="32"
+                                    height="22"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
 
-                                <path
-                                    d="M9.914 11.086l-2.829 2.829 8.914 8.914 8.914-8.914-2.828-2.828-6.086 6.086z"
-                                    fill="#0c0c0d"
-                                />
-                            </svg>
+                                    <path
+                                        d="M9.914 11.086l-2.829 2.829 8.914 8.914 8.914-8.914-2.828-2.828-6.086 6.086z"
+                                        fill="#0c0c0d"
+                                    />
+                                </svg>
 
-                        </button>
+                            </button>
+                        </div>
                     </div>
                 </div>
+
             </div>
 
         </div>
